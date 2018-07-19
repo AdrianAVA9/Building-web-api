ce740ca84dd6f393cf6da16878c0abe578e850f6
Adrián Vega <35713481+AdrianAVA9@users.noreply.github.com>
2018-07-19 19:01:51 +0200

    Initial
    Initial project

---
+++ ExpenseTracker.Persistance/ActionResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Persistance
+{
+    public class ActionResult: where <T> is class
+    {
+        public Exception Exception { get; set; }
+        public Result Result { get; set; }
+        public T Entity { get; set; }
+
+        public ActionResult(Exception ex, Result result, T entity):this(result,entity)
+        {
+            Exception = ex;
+        }
+
+        public ActionResult(Result result, T entity)
+        {
+            Result = result;
+            Entity = entity;
+        }
+    }
+}
---
+++ ExpenseTracker.Persistance/Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Persistance
+{
+    public enum Result
+    {
+        Updated = 1,
+        Created = 2,
+        Deleted = 3,
+        NotFound = 4,
+        Error = 5,
+    }
+}
